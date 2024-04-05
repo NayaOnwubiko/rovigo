7bbc2d07e94beb76e50ffd99d8ec5c73ab8b723d
Naya O <naya.onwubiko@gmail.com>
2024-04-04 23:26:32 -0400

    changed new request destination

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "https://rovigo-api.vercel.app/api/",
+  baseURL: "https://rovigo-api.onrender.com/api/",
   withCredentials: true,
 });
 
